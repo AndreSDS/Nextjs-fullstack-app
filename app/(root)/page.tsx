1268c493229ede4e13fb0b10bf889f492fe77e1f
andresds <andre_smiths@outlook.com>
2024-12-15 17:43:53 -0300

    config utils and add some styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <h1 className="text-2xl">Home</h1>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pick Your Startup, <br /> Connect With EntrePreneurs</h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pictures, and Get Noticed in Virtual Competitions.
+        </p>
+      </section>
+
+    </>
   );
 }
